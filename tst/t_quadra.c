9be10aa68e79d82cb4c0ee1ae2279e108f2cfe34
Maria Fernanda Morais do Amaral <mariafernandamamaral@gmail.com>
2026-06-15 20:20:15 -0300

    corrige comparacao numerica em testes de quadra
    Bug: os testes de quadra falhavam ao comparar valores numericos.
    
    Causa: o Unity do projeto esta com precisao double desabilitada.
    
    Solucao: trocar assercoes double por comparacoes float com tolerancia.

--- tst/t_quadra.c
+++ tst/t_quadra.c
@@ -15,10 +15,10 @@ void test_deve_criar_quadra_com_dados_geometricos(void)
 
     TEST_ASSERT_NOT_NULL(quadra);
     TEST_ASSERT_EQUAL_STRING("cep15", obter_cep_quadra(quadra));
-    TEST_ASSERT_EQUAL_DOUBLE(10.0, obter_x_quadra(quadra));
-    TEST_ASSERT_EQUAL_DOUBLE(20.0, obter_y_quadra(quadra));
-    TEST_ASSERT_EQUAL_DOUBLE(30.0, obter_largura_quadra(quadra));
-    TEST_ASSERT_EQUAL_DOUBLE(40.0, obter_altura_quadra(quadra));
+    TEST_ASSERT_FLOAT_WITHIN(0.0001, 10.0, obter_x_quadra(quadra));
+    TEST_ASSERT_FLOAT_WITHIN(0.0001, 20.0, obter_y_quadra(quadra));
+    TEST_ASSERT_FLOAT_WITHIN(0.0001, 30.0, obter_largura_quadra(quadra));
+    TEST_ASSERT_FLOAT_WITHIN(0.0001, 40.0, obter_altura_quadra(quadra));
 
     destruir_quadra(quadra);
 }
@@ -30,7 +30,7 @@ void test_deve_definir_estilo_da_quadra(void)
     TEST_ASSERT_NOT_NULL(quadra);
     definir_estilo_quadra(quadra, 2.5, "yellow", "black");
 
-    TEST_ASSERT_EQUAL_DOUBLE(2.5, obter_espessura_quadra(quadra));
+    TEST_ASSERT_FLOAT_WITHIN(0.0001, 2.5, obter_espessura_quadra(quadra));
     TEST_ASSERT_EQUAL_STRING("yellow", obter_preenchimento_quadra(quadra));
     TEST_ASSERT_EQUAL_STRING("black", obter_borda_quadra(quadra));
 
@@ -42,7 +42,7 @@ void test_deve_ter_estilo_padrao(void)
     Quadra quadra = criar_quadra("cep30", 1.0, 2.0, 3.0, 4.0);
 
     TEST_ASSERT_NOT_NULL(quadra);
-    TEST_ASSERT_EQUAL_DOUBLE(1.0, obter_espessura_quadra(quadra));
+    TEST_ASSERT_FLOAT_WITHIN(0.0001, 1.0, obter_espessura_quadra(quadra));
     TEST_ASSERT_EQUAL_STRING("none", obter_preenchimento_quadra(quadra));
     TEST_ASSERT_EQUAL_STRING("black", obter_borda_quadra(quadra));
 
@@ -57,11 +57,11 @@ void test_deve_rejeitar_cep_nulo(void)
 void test_deve_retornar_valores_neutros_para_quadra_nula(void)
 {
     TEST_ASSERT_NULL(obter_cep_quadra(NULL));
-    TEST_ASSERT_EQUAL_DOUBLE(0.0, obter_x_quadra(NULL));
-    TEST_ASSERT_EQUAL_DOUBLE(0.0, obter_y_quadra(NULL));
-    TEST_ASSERT_EQUAL_DOUBLE(0.0, obter_largura_quadra(NULL));
-    TEST_ASSERT_EQUAL_DOUBLE(0.0, obter_altura_quadra(NULL));
-    TEST_ASSERT_EQUAL_DOUBLE(0.0, obter_espessura_quadra(NULL));
+    TEST_ASSERT_FLOAT_WITHIN(0.0001, 0.0, obter_x_quadra(NULL));
+    TEST_ASSERT_FLOAT_WITHIN(0.0001, 0.0, obter_y_quadra(NULL));
+    TEST_ASSERT_FLOAT_WITHIN(0.0001, 0.0, obter_largura_quadra(NULL));
+    TEST_ASSERT_FLOAT_WITHIN(0.0001, 0.0, obter_altura_quadra(NULL));
+    TEST_ASSERT_FLOAT_WITHIN(0.0001, 0.0, obter_espessura_quadra(NULL));
     TEST_ASSERT_NULL(obter_preenchimento_quadra(NULL));
     TEST_ASSERT_NULL(obter_borda_quadra(NULL));
 }
