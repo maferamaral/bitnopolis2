0d9b256098e702c10ffce14cfd99f7b599469609
Maria Fernanda Morais do Amaral <mariafernandamamaral@gmail.com>
2026-06-15 19:53:55 -0300

    adiciona testes de quadra

---
+++ tst/t_quadra.c
@@ -0,0 +1,78 @@
+#include "unity.h"
+#include "quadra.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_deve_criar_quadra_com_dados_geometricos(void)
+{
+    Quadra quadra = criar_quadra("cep15", 10.0, 20.0, 30.0, 40.0);
+
+    TEST_ASSERT_NOT_NULL(quadra);
+    TEST_ASSERT_EQUAL_STRING("cep15", obter_cep_quadra(quadra));
+    TEST_ASSERT_EQUAL_DOUBLE(10.0, obter_x_quadra(quadra));
+    TEST_ASSERT_EQUAL_DOUBLE(20.0, obter_y_quadra(quadra));
+    TEST_ASSERT_EQUAL_DOUBLE(30.0, obter_largura_quadra(quadra));
+    TEST_ASSERT_EQUAL_DOUBLE(40.0, obter_altura_quadra(quadra));
+
+    destruir_quadra(quadra);
+}
+
+void test_deve_definir_estilo_da_quadra(void)
+{
+    Quadra quadra = criar_quadra("cep22", 1.0, 2.0, 3.0, 4.0);
+
+    TEST_ASSERT_NOT_NULL(quadra);
+    definir_estilo_quadra(quadra, 2.5, "yellow", "black");
+
+    TEST_ASSERT_EQUAL_DOUBLE(2.5, obter_espessura_quadra(quadra));
+    TEST_ASSERT_EQUAL_STRING("yellow", obter_preenchimento_quadra(quadra));
+    TEST_ASSERT_EQUAL_STRING("black", obter_borda_quadra(quadra));
+
+    destruir_quadra(quadra);
+}
+
+void test_deve_ter_estilo_padrao(void)
+{
+    Quadra quadra = criar_quadra("cep30", 1.0, 2.0, 3.0, 4.0);
+
+    TEST_ASSERT_NOT_NULL(quadra);
+    TEST_ASSERT_EQUAL_DOUBLE(1.0, obter_espessura_quadra(quadra));
+    TEST_ASSERT_EQUAL_STRING("none", obter_preenchimento_quadra(quadra));
+    TEST_ASSERT_EQUAL_STRING("black", obter_borda_quadra(quadra));
+
+    destruir_quadra(quadra);
+}
+
+void test_deve_rejeitar_cep_nulo(void)
+{
+    TEST_ASSERT_NULL(criar_quadra(NULL, 1.0, 2.0, 3.0, 4.0));
+}
+
+void test_deve_retornar_valores_neutros_para_quadra_nula(void)
+{
+    TEST_ASSERT_NULL(obter_cep_quadra(NULL));
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, obter_x_quadra(NULL));
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, obter_y_quadra(NULL));
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, obter_largura_quadra(NULL));
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, obter_altura_quadra(NULL));
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, obter_espessura_quadra(NULL));
+    TEST_ASSERT_NULL(obter_preenchimento_quadra(NULL));
+    TEST_ASSERT_NULL(obter_borda_quadra(NULL));
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_deve_criar_quadra_com_dados_geometricos);
+    RUN_TEST(test_deve_definir_estilo_da_quadra);
+    RUN_TEST(test_deve_ter_estilo_padrao);
+    RUN_TEST(test_deve_rejeitar_cep_nulo);
+    RUN_TEST(test_deve_retornar_valores_neutros_para_quadra_nula);
+    return UNITY_END();
+}
